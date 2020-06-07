aee53750aef9d1cc3b5451cd44930e96973fb15d
Joette Damo <jcdamo@gmail.com>
2020-06-07 15:46:16 -0700

    Hello commit 060720 Week 4 assignment

---
+++ app/src/main/java/com/example/hello/Constants.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+
+
+class Constants {
+
+    static final String KEY_ENTER_FULL_NAME = "enter_full_name";
+
+    static final String KEY_ENTER_AGE = "enter_age";
+
+    static final String KEY_ENTER_YOUR_DESCRIPTION = "enter_your_description";
+
+    static final String KEY_ENTER_OCCUPATION = "enter_occupation";
+
+}
+
